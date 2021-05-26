6adc9b1aa8135e413ba9556276b12bf5ff364509
Roland Nemes <nemesroland2@gmail.com>
2021-05-26 12:16:08 +0300

    yes

--- backup.sh
+++ backup.sh
@@ -13,7 +13,7 @@ case "$choice" in
 	  crontab -l > ~/.config/crontab.save.dinh; cp -rv ~/.config/crontab.save.dinh $DOTDIR/.config/
 	  sudo crontab -l > ~/.config/root-crontab.save.dinh; cp -rv ~/.config/root-crontab.save.dinh $DOTDIR/.config/
 	  echo "Backing up config files..."
-	  cp -rv ~/.config/{alacritty,dunst,git,gtk-2.0,gtk-3.0,i3,tmux,networkmanager-dmenu,nvim,mpd,ncmpcpp,picom,paru,ranger,sxhkd,X11,zsh,bash,zathura,user-dirs.dirs,wall.png} $DOTDIR/.config/
+	  cp -rv ~/.config/{alacritty,dunst,git,gtk-2.0,gtk-3.0,i3,tmux,neofetch,networkmanager-dmenu,nvim,mpd,ncmpcpp,picom,paru,ranger,sxhkd,X11,zsh,bash,zathura,user-dirs.dirs,wall.png} $DOTDIR/.config/
 	  echo "Backing up scripts..."
 	  cp -rv ~/.local/bin $DOTDIR/.local/
 	  echo "Backing up pandoc templates..."
